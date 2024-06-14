cce4d312e1532118cb3b1cbdfb9076a6e0270530
bmwill <bmwill@users.noreply.github.com>
2024-06-14 20:49:09 +0000

    deploy: b36b9818ab8111ebe498670368d798431a9e82d7

---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"diffy":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"diffy/struct.ApplyError.html\" title=\"struct diffy::ApplyError\">ApplyError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.79.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"struct\" href=\"diffy/struct.ParsePatchError.html\" title=\"struct diffy::ParsePatchError\">ParsePatchError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
